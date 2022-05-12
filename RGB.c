9ee0fc086177e858aa48949f3edb5620975ac657
hanilizalo23 <nelida_phm@hotmail.com>
2022-05-12 17:27:41 -0500

    Some RGB stuff added, no errors

--- RGB.c
+++ RGB.c
@@ -14,6 +14,11 @@
 #include "bits.h"
 #include "PIT.h"
 
+#define RESO 1U
+
+//Variable to save a maximum of 10 colors for the sequence
+static color_sequence_t g_colors_sequence [10] = {0};
+
 typedef enum {seqmin, seqmax = 2}range_seq; /**Enum for the limits of the sequence*/
 typedef enum {seqnw, seqsw2, seqsw3, seqsw23}number_seq; /**Enum for the values of the sequence*/
 typedef enum {color0, color1, color2}number_color; /**Enum for the three colors of every sequence*/
@@ -25,6 +30,61 @@ uint8_t sw3pin = 4;
 /**Variables for color and sequence*/
 uint8_t color = 0;
 uint8_t sequence = 0;
+
+//Counter to get 1 second
+static uint32_t g_counter_1s = 0x00;
+//Counter for the sequence of submenu 3
+static uint16_t g_sequence_counter = 0x00;
+//Variable that stores the total value of colors in the sequence
+static uint16_t g_sequence_total = 0x00;
+//Stores the colors in the sequence to use them on callbacks.
+static color_sequence_t *g_colors_seq = 0x00;
+//Stores if the sequence is specified to be stopped
+static continue_seq_t g_stop_flag = FALSE;
+
+//RGB codes of the colors for the sequence
+rgb_intensity_colors_t g_sequence_rgb_code[7] =
+{
+		{0,		0,		0	},			//NONE
+		{0, 	0, 	 	255	},			//BLUE
+		{255, 	0, 	 	0	},			//RED
+		{0, 	255, 	0	},			//GREEN
+		{255, 	255, 	0	},			//YELLOW
+		{255, 	0, 		255	},			//PURPLE
+		{255, 	255, 	255	},			//WHITE
+};
+
+/*For Sequence*/
+void RGB_led_start_sequence(color_sequence_t *colors,uint8_t sequence_total,continue_seq_t stop)
+{
+	g_colors_seq = colors;
+	g_sequence_total = sequence_total - RESO;
+	g_stop_flag = stop;
+	//Turn off the first color
+	RGB_led_set_color(&g_sequence_rgb_code[colors[0]]);
+	//Enables the interruption to get 1sec delay
+	RGB_led_delay_1s_init();
+}
+
+void RGB_led_change_seq_color(void)
+{
+	//If the sequence must stop and this is the last color...
+	if((g_stop_flag) && (g_sequence_total == g_sequence_counter))
+	{
+		g_sequence_counter = 0x00;
+		RGB_led_stop_sequence();
+	}
+	else if((!g_stop_flag) && (g_sequence_total == g_sequence_counter))
+	{
+		g_sequence_counter = 0x00;
+		RGB_led_set_color(&g_sequence_rgb_code[g_colors_seq[g_sequence_counter]]);
+	}
+	else
+	{
+		g_sequence_counter++;
+		RGB_led_set_color(&g_sequence_rgb_code[g_colors_seq[g_sequence_counter]]);
+	}
+}
 
 void SW3_cb(void) /**Setter for the callback of sw3 and both reading*/
 {
@@ -43,7 +103,7 @@ void SW3_cb(void) /**Setter for the callback of sw3 and both reading*/
 	/**An OR of the values*/
 	sw_read = sw2_read | sw3_read;
 
-	if (seqmin == sw_read) /**If the value is 0, it means, both swtiches are pressed, is the turn of the fourth sequence and cleaning the status*/
+	if (seqmin == sw_read) /**If the value is 0, it means, both switches are pressed, is the turn of the fourth sequence and cleaning the status*/
 	{
 		sequence = seqsw23;
 		GPIO_clear_irq_status(GPIO_A);
--- RGB.h
+++ RGB.h
@@ -9,6 +9,85 @@
 #ifndef RGB_H_
 #define RGB_H_
 
+/*Period of 1/500 (for PMW to control the RGB). Since we are using the system clock divided by 1,
+ * MOD = (1/500) / (1/60000000)*/
+#define FTM_CHV_RGB_INIT 	(0U)
+#define FTM_MOD_RGB 		(41820U)  	//Multiple of 255 to make the levels division easier
+#define FTM_MOD_500HZ		(42000U)	//To get approximately 500Hz, 0.002s
+
+//Channels for each color
+#define BLUE_CH 	CH_0
+#define RED_CH		CH_1
+#define GREEN_CH	CH_2
+
+#define RGB_MANUAL_BR_CHANGE (4182U) 	//FTM_MOD_RGB/10 (10%)
+//For intensity levels
+#define RGB_INTENSITY_MAX	(255U)
+#define RGB_INTENSITY_MIN	(0U)
+#define RGB_INTENSITY_GAIN (164U)
+//For PH levels convertions
+#define PH_LEVELS_MAX (14U)
+#define MAX_FREQ_PH   (10000U)
+//Number of repetitions to get 1 second
+#define COUNTS_ONE_SEC	(350000U)
+//ADC constants needed
+#define ADC_OFFSET		(40U)
+#define ADC_INCREMENT	(0.01961F)
+#define ADC_3_3_VOLTS	(168U)
+#define ADC_LVLS_PER_PH	(12U)
+#define ADC_SAMPLES_MAX (500U)
+#define ADC_INT_FACTOR	(10U)
+#define ADC_DIGS_NUMBER	(2U)
+
+//Basic colors of RGB
+typedef enum
+{
+	RED,
+	GREEN,
+	BLUE
+}color_value_t;
+
+//Colors to get on the sequence
+typedef enum
+{
+	NONE_RGB,
+	BLUE_RGB,
+	RED_RGB,
+	GREEN_RGB,
+	YELLOW_RGB,
+	PURPLE_RGB,
+	WHITE_RGB
+}color_sequence_t;
+
+//To know if the bright must be increased or decreased
+typedef enum
+{
+	INCREASE,
+	DECREASE
+}increase_or_decrease_br_t;
+
+//To choose whether the RGB sequence stops after ending the last color period, or it continues
+typedef enum
+{
+	CONTINUE_SEQ,
+	STOP_SEQ
+}continue_seq_t;
+
+//Structures to store the chosen FTM and channels for RGB
+typedef struct
+{
+	uint8_t ftm;
+	uint8_t channel;
+}rgb_bright_change_t;
+
+//Structure to store the RGB code
+typedef struct
+{
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+}rgb_intensity_colors_t;
+
 /**Setters for the sequence and color*/
 void SW2_cb(void);
 void SW3_cb(void);
